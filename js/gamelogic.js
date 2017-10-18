674859f4a7ce75469fd7662937547baef3ddc222
Alok Dethe <dethealok@gmail.com>
2017-10-18 15:56:33 +1300

    recent changes

---
+++ js/gamelogic.js
@@ -0,0 +1,24 @@
+//Game Logic
+
+;(function( $, global ){
+    
+    if(!$){
+        throw new Error("Jquery is required!");
+    }
+    
+    //Character function
+    function Character(){
+        
+    }
+    
+    //Update character position
+    Character.prototype.update = function(){
+        
+    }
+    
+    //Update character walk
+    Character.prototype.walk = function(){
+        
+    }
+    
+}(jQuery, this));
